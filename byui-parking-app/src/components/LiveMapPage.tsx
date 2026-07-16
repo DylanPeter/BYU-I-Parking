82e35064b9bcbe0572af9605097d736bf1a16c95
Dylan Petersen <petersendylan2000@gmail.com>
2026-07-16 17:46:58 -0600

    fixes

--- byui-parking-app/src/components/LiveMapPage.tsx
+++ byui-parking-app/src/components/LiveMapPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useParking } from "../context/ParkingContext";
 import type { ParkingLot } from "../data/parkingData";
 
